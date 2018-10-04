6f4be6f6ab8d70388f8fd884069aaad0740d9ea3
Stephen appiah <eunisaeseagmail.com>
2018-10-04 05:33:04 +0200

    langind page done , create mediaqueries but to implement and make all responvie , nav nad search box done.

---
+++ src/components/navigation/FrontNav.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="nav" id="frontNav">
+
+    <ul id="ul-logo">
+      <li><a href="/"><i class="fab fa-firstdraft"></i></a></li>
+    </ul>
+
+    <ul>
+      <li><a href="/post-job" id="post-job">Post a Job</a></li>
+      <li><a href="/singup">Sign Up</a></li>
+      <li><a href="/login">Login</a></li>
+    </ul>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class FrontNav extends Vue {
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="sass"></style>
---
+++ src/components/navigation/SearchBox.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container" id="searchbox">
+
+    <div class="input-container">
+      <input type="search" id="search" placeholder="eg. Software engineer " />
+      <button class="icon"><i class="fa fa-search"></i></button>
+    </div>
+
+    <div class="input-container">
+      <input type="search" id="search" placeholder="eg. Barcelona " />
+      <button class="icon"><i class="fas fa-map-marker-alt"></i></button>
+    </div>
+
+    <button type="button" name="findJob" class="button"> Find jobs </button>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class SearchBox extends Vue {
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="sass"></style>
