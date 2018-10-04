9ec7af66f56b0acad54958a0cb6db1afbb4fa1a4
Stephen appiah <eunisaeseagmail.com>
2018-10-04 15:30:32 +0200

    added a siple nav bar mobile meu for tempo useg

--- src/components/navigation/FrontNav.vue
+++ src/components/navigation/FrontNav.vue
@@ -5,7 +5,9 @@
       <li><a href="/"><i class="fab fa-firstdraft"></i></a></li>
     </ul>
 
-    <ul>
+    <i class="fas fa-bars" id="bar-menu"></i>
+    <ul id="menu">
+      <li><a><i class="far fa-times-circle" id="close-bar"></i></a></li>
       <li><a href="/post-job" id="post-job">Post a Job</a></li>
       <li><a href="/singup">Sign Up</a></li>
       <li><a href="/login">Login</a></li>
@@ -22,6 +24,35 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 @Component
 export default class FrontNav extends Vue {
 
+  public mounted(){
+
+    const opeNavBar : any = document.getElementById('bar-menu');
+    const closeNavBar : any = document.getElementById('close-bar');
+    const menuBar : any = document.getElementById('menu');
+
+    let open = false;
+
+    opeNavBar.addEventListener('click' , (e) => {
+
+      if(!open){
+        open = true
+        menuBar.style.display = "block"
+        closeNavBar.style.display = "block"
+      }
+
+    });
+
+    closeNavBar.addEventListener('click' , (e) => {
+
+      if(open){
+        open = false
+        menuBar.style.display = "none"
+
+      }
+
+    });
+
+  }
 }
 </script>
 
