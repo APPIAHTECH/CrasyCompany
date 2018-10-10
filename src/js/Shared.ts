bb7ef7f9dacbd1a40cd88f26ae63779716471646
Stephen appiah <eunisaeseagmail.com>
2018-10-10 20:08:31 +0200

    pages footer added and shared template created

---
+++ src/js/Shared.ts
@@ -0,0 +1,7 @@
+// This file contains global common functions
+// Author : Stephen Appiah
+// Date   : 10/10/2018
+
+export default class Shared{
+
+}
--- src/store.ts
+++ src/store.ts
@@ -6,6 +6,11 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
 
+    SearchBoxData : {
+      title: [],
+      locations : []
+    }
+    
   },
   mutations: {
 
